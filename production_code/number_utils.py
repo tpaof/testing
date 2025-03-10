ca63763a39533c3a960dfd1f19acde12848d808b
Teerepat Phakdee <teerepatphakdeeaof@gmail.com>
2025-03-11 01:03:17 +0700

    fix number utils

--- production_code/number_utils.py
+++ production_code/number_utils.py
@@ -1,10 +1,10 @@
 def is_prime_list(numbers):
+    if not numbers:
+        return True 
     for num in numbers:
-        if num <= 1:  
+        if num <= 1:
             return False
-        if num == 1:
-            return False
-        for n in range(2, num):
+        for n in range(2, int(num**0.5) + 1):
             if num % n == 0:
                 return False
     return True
--- tests/test_number_utils.py
+++ tests/test_number_utils.py
@@ -2,30 +2,39 @@ from production_code.number_utils import is_prime_list
 import unittest
 
 class PrimeListTest(unittest.TestCase):
-    def test_give_1_2_3_is_prime(self):
-        prime_list = [1, 2, 3]
-        is_prime = is_prime_list(prime_list)
-        self.assertFalse(is_prime)
-    
-    def test_all_prime_numbers(self):
-        prime_list = [2, 3, 5, 7]
-        is_prime = is_prime_list(prime_list)
-        self.assertTrue(is_prime)
-    
-    def test_contains_composite_numbers(self):
-        prime_list = [4, 6, 8, 9]
-        is_prime = is_prime_list(prime_list)
-        self.assertFalse(is_prime)
-    
-    def test_single_prime_number(self):
-        prime_list = [13]
-        is_prime = is_prime_list(prime_list)
-        self.assertTrue(is_prime)
-    
-    def test_empty_list(self):
-        prime_list = []
-        is_prime = is_prime_list(prime_list)
-        self.assertTrue(is_prime)
+    def test_2_3_5_7__should_return_true(self):
+        prime_list = [2, 3, 5, 7, 11, 13]
+        result = is_prime_list(prime_list)
+        self.assertTrue(result)
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_2_3_4_7__should_return_false(self):
+        prime_list = [2, 3, 4, 7, 10]
+        result = is_prime_list(prime_list)
+        self.assertFalse(result)
+
+    def test_empty_list__should_return_false(self):
+        prime_list = []
+        result = is_prime_list(prime_list)
+        self.assertTrue(result)  
+
+    def test_single_prime__should_return_true(self):
+        prime_list = [17]   
+        result = is_prime_list(prime_list)
+        self.assertTrue(result)
+
+    def test_single_non_prime__should_return_false(self):
+        prime_list = [9]
+        result = is_prime_list(prime_list)
+        self.assertFalse(result)
+
+    def test_negative_number__should_return_false(self):
+        prime_list = [-2, 3, 5]
+        result = is_prime_list(prime_list)
+        self.assertFalse(result)  
+
+    def test_list_with_one(self):
+        prime_list = [1]
+        result = is_prime_list(prime_list)
+        self.assertFalse(result)  
+
+
